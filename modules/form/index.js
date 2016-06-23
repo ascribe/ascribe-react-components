025088c09a649e3a86a877fe22e4d52961b0058a
Brett Sun <qisheng.brett.sun@gmail.com>
2016-06-23 16:34:25 +0200

    Move Buttons and collapsible Properties from react-utility-belt

---
+++ modules/form/index.js
@@ -0,0 +1,3 @@
+// Properties
+export CollapsibleCheckboxProperty from './properties/collapsible_checkbox_property';
+export CollapsibleProperty from './properties/collapsible_property';
---
+++ modules/index.js
@@ -0,0 +1,4 @@
+// Module reexports
+export * as Buttons from './buttons/';
+export * as Form from './form/';
+export * as Uploader from './uploader/';
